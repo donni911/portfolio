e6847fe3766d33d4b43f5cd186830db8ff149d46
donni911 <rodion2001vo@gmail.com>
2023-09-10 14:14:14 +0300

    reset profject

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,13 @@
 <template>
-  <section>
-    <div>
-      <h1>Hello my name is Rodion Voinarovskyi.</h1>
-      <h2>And here you can see my works</h2>
-    </div>
-    <ul>
-      <li v-for="item in portfolioItems" :key="item.id">
-        <a :href="item.url" target="_blank">{{ item.title }}</a>
-      </li>
-    </ul>
-  </section>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      portfolioItems: [
-        {
-          title: "Weather App",
-          url: "https://weather-app-43602.web.app/",
-        },
-        {
-          title: "To Do App",
-          url: "https://vue-todo-362bb.web.app/",
-        },
-        {
-          title: "Meals Library App",
-          url: "https://meal-library.web.app/",
-        },
-        {
-          title: "Adventure Notes App",
-          url: "https://adventure-notes.web.app/",
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
